747ca4328057a7f198ba0fe41b3182e9f373e2e7
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-31 14:49:19 -0300

    useEffects hook and SwipeableModules component added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SwipeableModules from '../components/SwipeableModules';
+import fetchModules from '../services/modulesService';
 
-const Home = () => <div />;
+const Home = () => {
+  const [modules, setModules] = useState([]);
+
+  useEffect(() => {
+    fetchModules().then(modules => setModules(modules));
+  }, []);
+
+  return <SwipeableModules modules={modules} />;
+};
 
 export default Home;
