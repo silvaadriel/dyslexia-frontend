fd8046e99f799b2b285519f91feffea65d93d7dd
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-01 15:36:28 -0300

    fetch questions and render in SwipeableQuestions component

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetchQuestions from '../services/lessonService'
+import SwipeableQuestions from '../components/SwipeableQuestions'
 
-const Lesson = () => <div />;
+const Lesson = () =>{
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions(questions).then(questions => setQuestions(questions));
+  }, [questions]);
+
+  return <SwipeableQuestions questions={questions} />;
+};
 
 export default Lesson;
