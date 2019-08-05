65b66df4b460c99eef2f1810309488cc4f0066cb
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-05 15:40:33 -0300

    MarkCorrectAnswer component created

---
+++ src/components/MarkCorrectAnswer.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import useGlobal from '../store';
+import { makeStyles } from '@material-ui/core/styles';
+import { VolumeUp } from '@material-ui/icons';
+import {
+  CardMedia,
+  Typography,
+  Grid,
+  IconButton,
+  Box,
+} from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+
+const useStyles = makeStyles({
+  img: {
+    maxWidth: '400px',
+    borderRadius: '10px',
+  },
+  toggleButtonRoot: {
+    color: '#4F4F4F',
+    border: '1px solid rgba(0, 0, 0, 0.25)',
+    borderRadius: '2px',
+    backgroundColor: '#FFFFFF',
+    '&:not(:first-child)': {
+      borderLeft: '1px solid rgba(0, 0, 0, 0.25)',
+      marginLeft: '0',
+    },
+    '&.Mui-selected': {
+      color: '#000000',
+      borderColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    '&.Mui-disabled': {
+      color: '#AAAAAA',
+      borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+  },
+  toggleButtonGroupRoot: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    height: '200px',
+    width: '350px',
+    backgroundColor: 'transparent',
+  },
+});
+
+const MarkCorrectAnswer = ({ step }) => {
+  const classes = useStyles();
+
+  const [globalState, globalActions] = useGlobal();
+  const [alternative, setAlternative] = useState('');
+
+  const handleChange = (event, newAlternative) => {
+    setAlternative(newAlternative);
+
+    if (newAlternative) globalActions.answer.setIsAnswered(true);
+    else globalActions.answer.setIsAnswered(false);
+  };
+
+  return (
+    <Grid
+      container
+      spacing={4}
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item container spacing={4} justify="center" alignItems="center">
+        <Grid item>
+          <Typography align="center" component="h1" variant="h4">
+            <Box fontWeight="600">{step.question}</Box>
+          </Typography>
+        </Grid>
+        <Grid item>
+          <IconButton title="Ouvir" aria-label="Escutar">
+            <VolumeUp />
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <CardMedia
+          className={classes.img}
+          component="img"
+          alt={step.img.alt}
+          image={step.img.url}
+          title={step.img.title}
+        />
+      </Grid>
+      <ToggleButtonGroup
+        value={alternative}
+        onChange={handleChange}
+        classes={{ root: classes.toggleButtonGroupRoot }}
+        exclusive
+      >
+        {step.alternatives.map((alternative, index) => (
+          <ToggleButton
+            key={index}
+            classes={{ root: classes.toggleButtonRoot }}
+            value={alternative}
+            // disabled={}
+          >
+            {alternative}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </Grid>
+  );
+};
+
+export default MarkCorrectAnswer;
