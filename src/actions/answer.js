9baa8b119b9a7b473a00d9511bfd35c3181a3522
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-06 15:23:31 -0300

    setIsVerified and verifyAnswer implemented

--- src/actions/answer.js
+++ src/actions/answer.js
@@ -1,3 +1,15 @@
 export const setIsAnswered = (store, isAnswered) => {
   store.setState({ isAnswered });
 };
+
+export const setIsVerified = (store, isVerified) => {
+  store.setState({ isVerified });
+};
+
+export const verifyAnswer = (store, question) => {
+  if (question.rightAnswer === store.state.currentAnswer) {
+    console.log('correto');
+    store.actions.answer.setIsVerified(true);
+  }
+  store.actions.answer.setIsVerified(true);
+};
