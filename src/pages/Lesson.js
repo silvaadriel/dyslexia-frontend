c7c26fd093711b8a1b3b6ca797012a7fd07c62b3
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-14 15:40:51 -0300

    Loading component implemented

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import useGlobal from '../store';
 import SwipeableQuestions from '../components/SwipeableQuestions';
+import Loading from '../components/Loading';
 
 const Lesson = props => {
   const [globalState, globalActions] = useGlobal();
@@ -11,7 +12,15 @@ const Lesson = props => {
     globalActions.lesson.fetchQuestions(params);
   }, [globalActions.lesson, params]);
 
-  return <SwipeableQuestions questions={globalState.questions} />;
+  return (
+    <>
+      {globalState.isLoading ? (
+        <Loading />
+      ) : (
+        <SwipeableQuestions questions={globalState.questions} />
+      )}
+    </>
+  );
 };
 
 export default Lesson;
