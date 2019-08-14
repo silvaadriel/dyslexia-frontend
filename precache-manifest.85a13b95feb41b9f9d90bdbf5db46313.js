43018fb14920ce32cb3b24eef3ae0715b2277e76
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-14 15:53:49 -0300

    Updates

---
+++ precache-manifest.85a13b95feb41b9f9d90bdbf5db46313.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32e3f2936dce98c016ee8dc88d6f9ac",
+    "url": "/dyslexia-frontend/index.html"
+  },
+  {
+    "revision": "a8f4e1c4c2c523472752",
+    "url": "/dyslexia-frontend/static/js/2.3e468c0c.chunk.js"
+  },
+  {
+    "revision": "77c0f55888f0dd794003",
+    "url": "/dyslexia-frontend/static/js/main.bef27b66.chunk.js"
+  },
+  {
+    "revision": "03ea934d8a1107876862",
+    "url": "/dyslexia-frontend/static/js/runtime~main.9f324e78.js"
+  }
+]);
